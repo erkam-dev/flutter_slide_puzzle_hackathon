ee43ee7b3f54d55644443160e35ebbd6a51f172e
MehmetErkam <erkamfb14@gmail.com>
2022-02-02 17:45:19 +0300

    Added puzzle functions and other statements

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_slide_puzzle/widgets/AnalyticsRow.dart';
 import 'package:flutter_custom_slide_puzzle/widgets/PuzzleGrid.dart';
@@ -11,18 +13,28 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   //Ekranda gözükecek sayılar
-  var sayilar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   //kaç hamle kullandığımızı tutan değişken
-  var hareket = 0;
+  var move = 0;
+
+  static const duration = const Duration(seconds: 1);
+  int secondsPassed = 0;
+  bool isActive = false;
+  Timer? timer;
 
   @override
   void initState() {
     super.initState();
-    sayilar.shuffle(); //Uygulama başlangıcında sayı dizisini karıştıran metod
+    numbers.shuffle(); //Uygulama başlangıcında sayı dizisini karıştıran metod
   }
 
   @override
   Widget build(BuildContext context) {
+    if (timer == null) {
+      timer = Timer.periodic(duration, (Timer t) {
+        startTime();
+      });
+    }
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -36,10 +48,7 @@ class _HomepageState extends State<Homepage> {
             //karıştır tuşu
             onPressed: () {
               setState(() {
-                sayilar.shuffle();
-                hareket = 0;
-                // secondsPassed = 0;
-                // isActive = false;
+                reset();
               });
             },
             icon: Icon(Icons.loop),
@@ -51,13 +60,103 @@ class _HomepageState extends State<Homepage> {
       body: ListView(
         //Hamle sayısı zaman ve Puzzle'ın bulunduğu yer
         children: [
-          AnalyticsRow(hareket: hareket), //analiz sırası
+          AnalyticsRow(
+            move: move,
+            secondsPassed: secondsPassed,
+          ), //analiz sırası
           PuzzleGrid(
             //Puzzle widget'ı
-            sayilar: sayilar,
+            sayilar: numbers,
+            click: clickGrid,
           ),
         ],
       ),
     );
+  }
+
+  void clickGrid(index) {
+    if (secondsPassed == 0) {
+      isActive = true;
+    }
+    if (index - 1 >= 0 && numbers[index - 1] == 0 && index % 4 != 0 ||
+        index + 1 < 16 && numbers[index + 1] == 0 && (index + 1) % 4 != 0 ||
+        (index - 4 >= 0 && numbers[index - 4] == 0) ||
+        (index + 4 < 16 && numbers[index + 4] == 0)) {
+      setState(() {
+        move++;
+        numbers[numbers.indexOf(0)] = numbers[index];
+        numbers[index] = 0;
+      });
+    }
+    checkWin();
+  }
+
+  void startTime() {
+    if (isActive) {
+      setState(() {
+        secondsPassed = secondsPassed + 1;
+      });
+    }
+  }
+
+  void reset() {
+    setState(() {
+      numbers.shuffle();
+      move = 0;
+      secondsPassed = 0;
+      isActive = false;
+    });
+  }
+
+  bool isSorted(List list) {
+    int prev = list.first;
+    for (var i = 1; i < list.length - 1; i++) {
+      int next = list[i];
+      if (prev > next) return false;
+      prev = next;
+    }
+    return true;
+  }
+
+  void checkWin() {
+    if (isSorted(numbers)) {
+      isActive = false;
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)), //this right here
+              child: Container(
+                height: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "You Win!!",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        width: 220.0,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Close",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          });
+    }
   }
 }
